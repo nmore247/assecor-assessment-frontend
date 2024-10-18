f785d303b98078a1a9c1178d4c62271eef073a0e
Nikhil <nikhilmore2011@hotmail.com>
2024-10-18 12:33:15 +0200

    add navbar and home page

--- frontend-app/src/app/app.routes.ts
+++ frontend-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: 'home', loadComponent: () => import("./home/home.component").then((m) => m.HomeComponent) }
+];
